b5edd65667032eef4065a49dbbdf8e5a5d8fe92a
Gustav <201507743@post.au.dk>
2018-09-24 12:11:15 +0200

    added c# folder

---
+++ c#/keepConsoleOpen.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
